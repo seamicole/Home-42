1501b77030562ab2355457ee86e2b36f12840330
Sean O'Leary <so-leary@c2r2s8.42singapore.sg>
2023-09-06 12:35:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,20 +14,32 @@ vimrc_file="$home_dir/.vimrc"
 # Path to your .vimrc file in the same directory as install.sh
 source_vimrc="$script_dir/.vimrc"
 
-# Check if .vimrc already exists
-if [ -e "$vimrc_file" ]; then
-    read -p "You already have a .vimrc file in your Home folder. Do you wish to overwrite it? (y/n): " overwrite_response
-
-    if [ "$overwrite_response" = "y" ] || [ "$overwrite_response" = "Y" ]; then
-        # Overwrite the existing .vimrc file
-        cp "$source_vimrc" "$vimrc_file"
-    else
-	# Skip copying of .vimrc file
-        echo "Skipping .vimrc installation."
-    fi
+# Check if the script directory is the same as the home directory
+if [ "$script_dir" = "$home_dir" ]; then
+    echo "Running PlugInstall..."
 else
-    # .vimrc doesn't exist, install it
-    cp "$source_vimrc" "$vimrc_file"
+    # Path to your .vimrc file in the same directory as install.sh
+    source_vimrc="$script_dir/.vimrc"
+
+    # Check if .vimrc already exists
+    if [ -e "$vimrc_file" ]; then
+        read -p "You already have a .vimrc file in your Home folder. Do you wish to overwrite it? (y/n): " overwrite_response
+
+        if [ "$overwrite_response" = "y" ] || [ "$overwrite_response" = "Y" ]; then
+            # Overwrite the existing .vimrc file
+            echo "Overwriting .vimrc..."
+            cp "$source_vimrc" "$vimrc_file"
+            echo "The .vimrc file has been overwritten."
+        else
+            echo "Skipping .vimrc installation."
+        fi
+    else
+        # .vimrc doesn't exist, install it
+        echo "Installing .vimrc..."
+        cp "$source_vimrc" "$vimrc_file"
+        echo "The .vimrc file has been installed."
+        echo "Running PlugInstall..."
+    fi
 fi
 
 # Change to Home directory
@@ -38,3 +50,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 
 # Run PlugInstall
 vim +'PlugInstall --sync' +qa
+
+# Print success message
+echo "Vim configured successfully!"
